6d33e7baff974ac9b16821e4cf327d1fce924b66
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-12 10:22:00 +0545

    Feat: Added Splash Screen
    Added Splash Screen

---
+++ app/src/main/java/com/elvin/expense_analyzer/ui/activity/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.elvin.expense_analyzer.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}

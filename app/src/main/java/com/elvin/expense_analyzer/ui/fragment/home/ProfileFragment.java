3af0708ab30de6cf740caffde284073218d5e171
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-07 00:06:59 +0545

    Refactor: Changed `HomeFragment` to `ProfileFragment`

--- app/src/main/java/com/elvin/expense_analyzer/ui/fragment/home/ProfileFragment.java
+++ app/src/main/java/com/elvin/expense_analyzer/ui/fragment/home/ProfileFragment.java
@@ -11,13 +11,13 @@ import androidx.fragment.app.Fragment;
 
 import com.elvin.expense_analyzer.R;
 
-public class HomeFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         return root;
     }

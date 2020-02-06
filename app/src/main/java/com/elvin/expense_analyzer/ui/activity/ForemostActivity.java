9f393681b6921e465aa0498e1be444c08bc9a7e5
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-07 00:56:37 +0545

    Refactor: Change action bar type to `NoActionBar`

--- app/src/main/java/com/elvin/expense_analyzer/ui/activity/ForemostActivity.java
+++ app/src/main/java/com/elvin/expense_analyzer/ui/activity/ForemostActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.elvin.expense_analyzer.R;
@@ -20,11 +19,7 @@ public class ForemostActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_profile, R.id.navigation_dashboard, R.id.navigation_settings)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 

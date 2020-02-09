173fd1ba70d4b4fffb5496cb47779a30bf3c7986
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-09 17:44:53 +0545

    Feature: Added category count in Category Activity

--- app/src/main/java/com/elvin/expense_analyzer/ui/activity/CategoryActivity.java
+++ app/src/main/java/com/elvin/expense_analyzer/ui/activity/CategoryActivity.java
@@ -1,16 +1,65 @@
 package com.elvin.expense_analyzer.ui.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.elvin.expense_analyzer.R;
+import com.elvin.expense_analyzer.endpoint.model.dto.CategoryCountDto;
+import com.elvin.expense_analyzer.endpoint.model.dto.ResponseDto;
+import com.elvin.expense_analyzer.endpoint.service.CategoryService;
+import com.elvin.expense_analyzer.utils.RetrofitUtils;
+import com.elvin.expense_analyzer.utils.SharedPreferencesUtils;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CategoryActivity extends AppCompatActivity {
+
+    private TextView tvIncomeCategoryCount, tvExpenseCategoryCount;
+    private CategoryService categoryService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        this.categoryService = RetrofitUtils.getRetrofit().create(CategoryService.class);
+
+        this.bindControls();
+        this.loadCounts();
+    }
+
+    private void loadCounts() {
+        Call<ResponseDto<CategoryCountDto>> call =
+                this.categoryService.statusCount(SharedPreferencesUtils.getAuthToken(getApplicationContext()));
+        call.enqueue(new Callback<ResponseDto<CategoryCountDto>>() {
+            @Override
+            public void onResponse(Call<ResponseDto<CategoryCountDto>> call, Response<ResponseDto<CategoryCountDto>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(CategoryActivity.this, "Failed to load categories count", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                CategoryCountDto categoryCountDto = response.body().getDetail();
+                tvIncomeCategoryCount.setText(String.valueOf(categoryCountDto.getIncomeCount()));
+                tvExpenseCategoryCount.setText(String.valueOf(categoryCountDto.getExpenseCount()));
+            }
+
+            @Override
+            public void onFailure(Call<ResponseDto<CategoryCountDto>> call, Throwable t) {
+                Log.e("Category Count", "Failed to load categories count", t);
+                Toast.makeText(CategoryActivity.this, "Failed to load categories count", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void bindControls() {
+        this.tvIncomeCategoryCount = findViewById(R.id.tvIncomeCategoryCount);
+        this.tvExpenseCategoryCount = findViewById(R.id.tvExpenseCategoryCount);
     }
 }

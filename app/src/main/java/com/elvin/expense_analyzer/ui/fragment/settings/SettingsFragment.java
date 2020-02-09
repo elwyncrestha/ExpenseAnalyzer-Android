9ea5b7d42392d45fa90cdc6d2109e2fbf7a5d63b
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-09 15:57:35 +0545

    Feature: Added Category Activity Basic Layout

--- app/src/main/java/com/elvin/expense_analyzer/ui/fragment/settings/SettingsFragment.java
+++ app/src/main/java/com/elvin/expense_analyzer/ui/fragment/settings/SettingsFragment.java
@@ -1,21 +1,49 @@
 package com.elvin.expense_analyzer.ui.fragment.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.elvin.expense_analyzer.R;
+import com.elvin.expense_analyzer.ui.activity.CategoryActivity;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+
+    private CardView cardCategory, cardPaymentMethod, cardStatus, cardAbout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        this.cardCategory = root.findViewById(R.id.cardCategory);
+        this.cardPaymentMethod = root.findViewById(R.id.cardPaymentMethod);
+        this.cardStatus = root.findViewById(R.id.cardStatus);
+        this.cardAbout = root.findViewById(R.id.cardAbout);
+
+        this.cardCategory.setOnClickListener(this);
+        this.cardPaymentMethod.setOnClickListener(this);
+        this.cardStatus.setOnClickListener(this);
+        this.cardAbout.setOnClickListener(this);
+
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cardCategory:
+                startActivity(new Intent(getContext(), CategoryActivity.class));
+                break;
+            default:
+                Toast.makeText(getContext(), "Invalid action", Toast.LENGTH_SHORT).show();
+        }
     }
 }
